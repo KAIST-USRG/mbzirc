58ef8615490404f2eec1325b8a51b4e16990b1e0
Donghee Han <hdh7485@gmail.com>
2019-11-06 22:23:04 +0900

    Add yaw control

--- mbzirc_wheel_control/script/hub_motor_control.py
+++ mbzirc_wheel_control/script/hub_motor_control.py
@@ -10,32 +10,50 @@ import math
 def twist_cb(data):
     global odrv0, odrv1
 
-    print(odrv0.axis0.error)
-    print(odrv0.axis1.error)
-    print(odrv1.axis0.error)
-    print(odrv1.axis1.error)
-
-    #if odrv0.axis0.error != 0 or odrv0.axis1.error != 0:
-    #    odrv0.axis0.error = 0
-    #    odrv0.axis1.error = 0
-    #    odrv0.save_configuration()
-    #    rospy.loginfo("odrv0 error")
-    #if odrv1.axis0.error != 0 or odrv1.axis1.error != 0:
-    #    odrv1.axis0.error = 0
-    #    odrv1.axis1.error = 0
-    #    odrv1.save_configuration()
-    #    rospy.loginfo("odrv1 error")
+    rospy.logdebug(odrv0.axis0.error)
+    rospy.logdebug(odrv0.axis1.error)
+    rospy.logdebug(odrv1.axis0.error)
+    rospy.logdebug(odrv1.axis1.error)
 
     radius = 0.127
-    rpm = math.sqrt(data.linear.x**2 + data.linear.y**2) * 30 / (radius * math.pi)
-    rpm_gain = rpm * 3 / 2
+    if data.angular.z > 0.3:
+        rpm = -50
+        rpm_rf = rpm
+        rpm_lf = -rpm
+        rpm_rb = rpm
+        rpm_lb = -rpm
+    elif data.angular.z < -0.3:
+        rpm = 50
+        rpm_rf = rpm
+        rpm_lf = -rpm
+        rpm_rb = rpm
+        rpm_lb = -rpm
+    else:
+        speed = math.sqrt(data.linear.x**2 + data.linear.y**2)
+        if speed < 0.05:
+            rpm = 0
+            rpm_rf = rpm
+            rpm_lf = rpm
+            rpm_rb = rpm
+            rpm_lb = rpm
+        else:
+            rpm = math.sqrt(data.linear.x**2 + data.linear.y**2) * 30 / (radius * math.pi)
+            rpm_rf = rpm
+            rpm_lf = rpm
+            rpm_rb = rpm
+            rpm_lb = rpm
 
-    odrv0.axis0.controller.vel_setpoint = rpm_gain
-    odrv0.axis1.controller.vel_setpoint = rpm_gain
-    odrv1.axis0.controller.vel_setpoint = rpm_gain
-    odrv1.axis1.controller.vel_setpoint = rpm_gain
+    rpm_gain_rf = rpm_rf * 3 / 2
+    rpm_gain_lf = rpm_lf * 3 / 2
+    rpm_gain_rb = rpm_rb * 3 / 2
+    rpm_gain_lb = rpm_lb * 3 / 2
 
-    rospy.loginfo(str(rpm) + ' '\
+    odrv0.axis0.controller.vel_setpoint = rpm_gain_rf
+    odrv0.axis1.controller.vel_setpoint = rpm_gain_lf
+    odrv1.axis0.controller.vel_setpoint = rpm_gain_lb
+    odrv1.axis1.controller.vel_setpoint = rpm_gain_rb
+
+    rospy.logdebug(str(rpm) + ' '\
                   + str(data.linear.x) + ' '\
                   + str(data.linear.y) + ' '\
                   + str(data.angular.z))
@@ -48,7 +66,9 @@ def listener():
 if __name__ == '__main__':
     global odrv0, odrv1 
     odrv0 = odrive.find_any(serial_number='20803592524B')
+    rospy.loginfo("20803592524B odrv0 connected!")
     odrv1 = odrive.find_any(serial_number='20633588524B')
+    rospy.loginfo("20633588524B odrv1 connected!")
     odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
     odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
     odrv1.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
--- mbzirc_wheel_control/src/wheel_control.cpp
+++ mbzirc_wheel_control/src/wheel_control.cpp
@@ -6,19 +6,37 @@
 double const RESOLUTION = 1002866;
 const double RAD_DEG = 57.2957951;
 int dynamixel_degree = 0;
+int dynamixel_degree_lf = 0;
+int dynamixel_degree_rf = 0;
+int dynamixel_degree_lb = 0;
+int dynamixel_degree_rb = 0;
+double point_turn_angle = atan2(1055, 655) * RAD_DEG;
 
 void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& twist_msg) {
-  double x = twist_msg->linear.x;
-  double y = twist_msg->linear.y;
-  double atan_degree = atan2(y, x) * RAD_DEG;
-  dynamixel_degree = atan_degree / 360.0 * RESOLUTION; 
-  if(atan_degree < 0.001 && atan_degree > -0.001)
-    dynamixel_degree = 0;
-  else if(dynamixel_degree > 501433) {
-    dynamixel_degree = -501433 + dynamixel_degree - 501433;
-  }
+  if(twist_msg->angular.z > 0.3 || twist_msg->angular.z < -0.3){
+    dynamixel_degree = point_turn_angle / 360.0 * RESOLUTION; 
+    if(dynamixel_degree > 501433) dynamixel_degree = -501433 + dynamixel_degree - 501433;
 
-  //ROS_INFO("degree: %lf, dynamixel_input: %d", atan_degree, dynamixel_degree);
+    dynamixel_degree_lf = -dynamixel_degree;
+    dynamixel_degree_rf = -dynamixel_degree;
+    dynamixel_degree_lb = dynamixel_degree;
+    dynamixel_degree_rb = dynamixel_degree;
+  }
+  else{
+    double x = twist_msg->linear.x;
+    double y = -twist_msg->linear.y;
+    double atan_degree = atan2(y, x) * RAD_DEG;
+    dynamixel_degree = atan_degree / 360.0 * RESOLUTION; 
+    double speed = std::sqrt(x*x + y*y);
+    if(speed < 0.001 && speed > -0.001) dynamixel_degree = 0;
+    else if(dynamixel_degree > 501433) dynamixel_degree = -501433 + dynamixel_degree - 501433;
+    dynamixel_degree_lf
+    = dynamixel_degree_rf
+    = dynamixel_degree_lb
+    = dynamixel_degree_rb
+    = dynamixel_degree;
+  }
+  ROS_DEBUG("dynamixel_input: %d", dynamixel_degree);
 }
 
 int main(int argc, char **argv) {
@@ -39,12 +57,12 @@ int main(int argc, char **argv) {
     srv.request.command = item_command;
     srv.request.id = 1;
     srv.request.addr_name = item_addr;
-    srv.request.value = dynamixel_degree;
+    srv.request.value = dynamixel_degree_rb;
 
     if (client.call(srv))
     {
-      //ROS_INFO("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
-      //ROS_INFO("receive result : %d", (bool)srv.response.comm_result);
+      ROS_DEBUG("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
+      ROS_DEBUG("receive result : %d", (bool)srv.response.comm_result);
     }
     else
     {
@@ -55,12 +73,12 @@ int main(int argc, char **argv) {
     srv.request.command = item_command;
     srv.request.id = 2;
     srv.request.addr_name = item_addr;
-    srv.request.value = dynamixel_degree;
+    srv.request.value = dynamixel_degree_lf;
 
     if (client.call(srv))
     {
-      //ROS_INFO("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
-      //ROS_INFO("receive result : %d", (bool)srv.response.comm_result);
+      ROS_DEBUG("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
+      ROS_DEBUG("receive result : %d", (bool)srv.response.comm_result);
     }
     else
     {
@@ -71,12 +89,12 @@ int main(int argc, char **argv) {
     srv.request.command = item_command;
     srv.request.id = 3;
     srv.request.addr_name = item_addr;
-    srv.request.value = dynamixel_degree;
+    srv.request.value = dynamixel_degree_rf;
 
     if (client.call(srv))
     {
-      //ROS_INFO("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
-      //ROS_INFO("receive result : %d", (bool)srv.response.comm_result);
+      ROS_DEBUG("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
+      ROS_DEBUG("receive result : %d", (bool)srv.response.comm_result);
     }
     else
     {
@@ -87,12 +105,12 @@ int main(int argc, char **argv) {
     srv.request.command = item_command;
     srv.request.id = 4;
     srv.request.addr_name = item_addr;
-    srv.request.value = dynamixel_degree;
+    srv.request.value = dynamixel_degree_lb;
 
     if (client.call(srv))
     {
-      //ROS_INFO("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
-      //ROS_INFO("receive result : %d", (bool)srv.response.comm_result);
+      ROS_DEBUG("send ID and Position Value : %u, %d", (uint8_t)srv.request.id, (int32_t)srv.request.value);
+      ROS_DEBUG("receive result : %d", (bool)srv.response.comm_result);
     }
     else
     {
