a567ddf249a6811d958094159cb0f28d2b7c468c
Donghee Han <hdh7485@gmail.com>
2020-01-31 19:21:23 +0900

    Add calc_twist and is_arrived methods

--- mbzirc_goto_brick/scripts/goto_brick.py
+++ mbzirc_goto_brick/scripts/goto_brick.py
@@ -14,9 +14,14 @@ class GotoBrick:
         self.default_param = rospy.get_param('default_param', 1)
         self.service_control = rospy.get_param('service_control', True)
 
+        self.raw_x = 0.0
+        self.raw_y = 0.0
+        self.raw_yaw = 0.0
         self.destination_x = 0.0
         self.destination_y = 0.0
         self.destination_yaw = 0.0
+        self.is_arrived = False
+        self.result_cmd_vel = Twist()
 
         self.twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('/goal_position', Pose, self.pose_callback, queue_size=10)
@@ -36,28 +41,40 @@ class GotoBrick:
             pose_msg.pose.orientation.y,
             pose_msg.pose.orientation.z,
             pose_msg.pose.orientation.w)
-
-        self.destination_x = pose_msg.pose.position.x
-        self.destination_y = pose_msg.pose.position.y
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        self.raw_yaw = euler[2]
+        self.raw_x = pose_msg.pose.position.x
+        self.raw_y= pose_msg.pose.position.y
 
         rospy.loginfo('x:{} y:{} yaw:{}'.format(self.destination_x, self.destination_y, self.destination_yaw))
 
     def calc_twist(self):
+        if not detected():
+            #point turn
+
+        else:
+            #goto brick
+
         
     def publish_twist(self):
-        self.result_cmd_vel = self.calc_twist()
-        self.twist_pub.publish(result_cmd_vel)
+        self.twist_pub.publish(self.result_cmd_vel)
 
     def pose_filter(self):
         pass
 
+    def is_arrived(self):
+        return False
+
     def run(self):
         r = rospy.Rate(10)
         is_arrived = False
-        while not is_arrived:
-            self.pub.publish(self.value)
+        while not rospy.is_shutdown:
+            if self.is_arrived():
+                break
+            self.calc_twist()
+            self.publish_twist()
             r.sleep()
-        return 'success'
+        #return 'success'
 
 if __name__ == "__main__":
     gotoBrick = GotoBrick()
